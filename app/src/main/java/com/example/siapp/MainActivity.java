b847ca11cfcd7e948c6263c22a97b16b5ac7deb8
Hasthiya <hardstudio.rambukwella@gmail.com>
2017-12-20 00:12:21 +0530

    Message view done

--- app/src/main/java/com/example/siapp/MainActivity.java
+++ app/src/main/java/com/example/siapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.siapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton showMessageButton = (ImageButton) findViewById(R.id.showMessageButton);
+        final TextView message = (TextView) findViewById(R.id.messageText);
+
+        showMessageButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MessageActivity.class);
+                intent.putExtra("Message",message.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
