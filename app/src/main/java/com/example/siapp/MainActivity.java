501137d4da14ef25c81cbc3b3eed36b63a06df65
Hasthiya <hardstudio.rambukwella@gmail.com>
2017-12-21 00:32:02 +0530

    text to speech added

--- app/src/main/java/com/example/siapp/MainActivity.java
+++ app/src/main/java/com/example/siapp/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.siapp;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements
+        TextToSpeech.OnInitListener {
 
 
-
+    private TextToSpeech tts;
+    private EditText messagetext;
+    private ImageButton readMessgae;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tts = new TextToSpeech(this, this);
+        readMessgae = (ImageButton) findViewById(R.id.readButton);
+        messagetext = (EditText) findViewById(R.id.messageText);
         ImageButton showMessageButton = (ImageButton) findViewById(R.id.showMessageButton);
         final TextView message = (TextView) findViewById(R.id.messageText);
 
@@ -30,6 +41,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        readMessgae.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
+
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    private void speakOut() {
+
+        String text = messagetext.getText().toString();
+
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 
     @Override
@@ -65,4 +102,22 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                readMessgae.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
 }
