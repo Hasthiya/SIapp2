947958a4c8733f2b7bfd016a9317c23d808a66e7
Hasthiya <hardstudio.rambukwella@gmail.com>
2017-12-20 22:10:49 +0530

    Minor changes

--- app/src/main/java/com/example/siapp/MainActivity.java
+++ app/src/main/java/com/example/siapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.siapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -27,5 +29,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.item1:
+                //your action
+                break;
+            case R.id.item2:
+                //your action
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
 }
